09f14b7499cb89b22d5e64cd0f48cd8a6af56fd6
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-10-09 11:47:08 +0200

    Complete the import database command line tool.

--- Importer/main.m
+++ Importer/main.m
@@ -9,9 +9,46 @@
 #import <Foundation/Foundation.h>
 #import <MagicalRecord/MagicalRecord.h>
 
+#import "Glider.h"
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Importing database!");
+        NSLog(@"🚀 Importing database started.");
+        
+        // Prepare database.
+        NSFileManager* fileManager = [NSFileManager defaultManager];
+        NSString *path = [@"~/Desktop/preloaded.sqlite" stringByExpandingTildeInPath];
+
+        // Removing file if needed.
+        if ([fileManager fileExistsAtPath:path]) {
+            if ([fileManager removeItemAtPath:path error:nil]) {
+                NSLog(@"🚀 Removed the preloaded database file.");
+            }
+        }
+        
+        // Setup MagicalRecord.
+        [MagicalRecord setLoggingLevel:MagicalRecordLoggingLevelOff];
+        [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreAtURL:[NSURL fileURLWithPath:path]];
+        
+        // Import gliders.
+        [MagicalRecord saveWithBlockAndWait:^(NSManagedObjectContext *localContext) {
+            NSLog(@"✈️ Importing glider OO-YBR.");
+            Glider *ventus = [Glider MR_createEntityInContext:localContext];
+            ventus.immatriculation = @"OO-YBR";
+            ventus.name = @"Schempp-Hirth Ventus cT";
+            
+            NSLog(@"✈️ Importing glider OO-YEI.");
+            Glider *ls = [Glider MR_createEntityInContext:localContext];
+            ls.immatriculation = @"OO-YEI";
+            ls.name = @"Rolladen-Schneider LS7";
+            
+            NSLog(@"✈️ Importing glider OO-YMR.");
+            Glider *discus = [Glider MR_createEntityInContext:localContext];
+            discus.immatriculation = @"OO-YMR";
+            discus.name = @"Schempp-Hirth Discus 2b";
+        }];
+        
+        NSLog(@"🚀 Importing database ended.");
     }
     return 0;
 }
